e7657c1bfb97ef8c260c54ef31b28b115cf87b88
rlittle <rlittle@ubuntu>
2011-09-18 11:01:43 -0400

    Updated the way the edit button is positioned.

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -6,7 +6,7 @@ module SiteHelper
         if session[:user_id] then
           user=User.find(session[:user_id])
         if user.roles.detect{|role|((role.name=="Admin") | (role.name=="Site Owner"))} then
-          returnval="<div id=\"edit-product\">"
+          returnval="<div id=\"edit-pages\">"
           returnval=returnval+link_to(image_tag("edit.png",:border=>"0", :class=>"imagebutton"),:controller => 'pages', :id =>page.id ,  :action => 'edit')
           returnval=returnval + "</div>"
          end
