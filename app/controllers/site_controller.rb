e6d449f549afd7cc9bebb85d8b311738a08f0232
rlittle <rlittle@ubuntu>
2012-01-05 02:14:55 -0500

    Added check for duplicate before adding property to properties object.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -44,26 +44,29 @@ class SiteController < ApplicationController
       save=true
       doc.css(".resultItemContainer").each do |item|
         save=true
-        a = Property.new
-        a.title = item.at_css(".resultsPrice b").text rescue save=false
-        a.price = item.at_css(".resultsPrice:nth-child(3) font").text[/\$[0-9\.\,]+/] rescue "nothing"
-        a.municipality = item.at_css(".resultsDetailsTable tr:nth-child(2) td:nth-child(2)").text rescue "nothing"
-        a.bedrooms = item.at_css("#contentCell tr:nth-child(3) td:nth-child(2)").text rescue "nothing"
-        a.fullbaths = item.at_css("tr:nth-child(4) td:nth-child(2)").text rescue ""
-        a.halfbaths = item.at_css("tr:nth-child(4) td:nth-child(4)").text rescue ""
-        a.acreage = item.at_css("tr:nth-child(5) td:nth-child(2)").text rescue ""
-        a.squarefeet = item.at_css(".resultsDetailsTable tr:nth-child(2) td:nth-child(4)").text rescue ""
-        a.yearbuilt = item.at_css("#contentCell tr:nth-child(3) td:nth-child(4)").text rescue ""
-        a.exterior = item.at_css("tr:nth-child(6) td:nth-child(2)").text rescue ""
+        title= item.at_css(".resultsPrice b").text rescue save=false
+        if Property.find_by_title(title).blank? then
+          a = Property.new
+          a.title = title
+          a.price = item.at_css(".resultsPrice:nth-child(3) font").text[/\$[0-9\.\,]+/] rescue "nothing"
+          a.municipality = item.at_css(".resultsDetailsTable tr:nth-child(2) td:nth-child(2)").text rescue "nothing"
+          a.bedrooms = item.at_css("#contentCell tr:nth-child(3) td:nth-child(2)").text rescue "nothing"
+          a.fullbaths = item.at_css("tr:nth-child(4) td:nth-child(2)").text rescue ""
+          a.halfbaths = item.at_css("tr:nth-child(4) td:nth-child(4)").text rescue ""
+          a.acreage = item.at_css("tr:nth-child(5) td:nth-child(2)").text rescue ""
+          a.squarefeet = item.at_css(".resultsDetailsTable tr:nth-child(2) td:nth-child(4)").text rescue ""
+          a.yearbuilt = item.at_css("#contentCell tr:nth-child(3) td:nth-child(4)").text rescue ""
+          a.exterior = item.at_css("tr:nth-child(6) td:nth-child(2)").text rescue ""
         
          
         
-        photo = item.at_css(".resultsPhoto") rescue "nothing"
-        a.photo_url = photo[:src] rescue "nothing"
-        if save then a.save end
+          photo = item.at_css(".resultsPhoto") rescue "nothing"
+          a.photo_url = photo[:src] rescue "nothing"
+          if save then a.save end
       
-        puts "#{a.title} - #{a.price}- #{a.municipality} #{a.photo[:alt]} #{a.photo[:src]}" rescue "nothing"
-        #puts item.at_css(".prodLink")[:href]
+          puts "#{a.title} - #{a.price}- #{a.municipality} #{a.photo[:alt]} #{a.photo[:src]}" rescue "nothing"
+          #puts item.at_css(".prodLink")[:href]
+        end
       end
     end
     
